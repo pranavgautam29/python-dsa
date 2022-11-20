15cbc2a949eacbb723eeab6c9f0b0d305405f0ae
Pranav Gautam <95767267+pranavgautam29@users.noreply.github.com>
2022-11-20 18:03:55 +0530

    Queue in python
    queue in python using linked lists with methods of enqueue and dequeue

---
+++ queue-ds.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+    
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            temp = self.first 
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+myQueue = Queue(2)
+myQueue.enqueue(3)
+myQueue.enqueue(21)
+myQueue.dequeue()
+myQueue.print_queue()
